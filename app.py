2759bafecd92ab824d0d2b242d008a1865a0c6ed
Arbaz <arbazkhan772001@gmail.com>
2024-09-02 18:27:38 +0530

    Initial commit

--- app.py
+++ app.py
@@ -1,26 +1,24 @@
-import streamlit as st
+from flask import Flask, request, render_template
 import pickle
 import nltk
 from nltk.corpus import stopwords
 import string
 from nltk.stem.porter import PorterStemmer
+
+app = Flask(__name__)
+
 ps = PorterStemmer()
 
+# Function to preprocess the text
 def transform_text(text):
-    
-    # Lower case
     text = text.lower()
-    
-    # Tokenization
     text = nltk.word_tokenize(text)
     
-    # Removing special characters
     y = []
     for i in text:
         if i.isalnum():
             y.append(i)
 
-    # Removing stop words and punctuation
     text = y[:]
     y.clear()
 
@@ -28,7 +26,6 @@ def transform_text(text):
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
             
-    # stemming
     text = y[:]
     y.clear()
 
@@ -37,25 +34,34 @@ def transform_text(text):
     
     return " ".join(y)
 
+# Load the pre-trained model and vectorizer
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-st.title ("SMS Spam Detector")
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-input_sms = st.text_input("Enter msg here...")
-
-if st.button('Check'):
+@app.route('/predict', methods=['POST'])
+def predict():
+    input_sms = request.form.get('sms')
+    
     # 1. Preprocess
     transformed_sms = transform_text(input_sms)
 
-    # 2. vectorize
+    # 2. Vectorize
     vector_input = tfidf.transform([transformed_sms])
 
-    # 3. predict
+    # 3. Predict
     result = model.predict(vector_input)[0]
 
-    # 4. display
+    # 4. Display the result
     if result == 1:
-        st.header("Spam")
+        prediction = "Spam"
     else:
-        st.header("Not Spam")
+        prediction = "Not Spam"
+    
+    return render_template('index.html', prediction_text='Message is: {}'.format(prediction))
+
+if __name__ == "__main__":
+    app.run(debug=True)
